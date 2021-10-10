042049b2eb010a98bf080f3f96588c0ce7bb734e
aroary <71286008+aroary@users.noreply.github.com>
2021-10-10 14:26:19 -0400

    colomn of badges

--- badges/badges.js
+++ badges/badges.js
@@ -40,5 +40,7 @@ badges.forEach(badge => {
     img.src = badge.img;
     img.alt = badge.alt;
     link.appendChild(img);
+    const br = document.createElement("br");
     document.getElementsByTagName("main")[0].appendChild(link);
+    document.getElementsByTagName("main")[0].appendChild(br);
 });
