7115ddfb4c0c973e348aa3c96927f006eaf4d9e7
aroary <meanchemshkedi@gmail.com>
2022-01-19 10:43:07 -0500

    discord webhooks

---
+++ discord/webhook.js
@@ -0,0 +1,12 @@
+function sendMessage() {
+    const request = new XMLHttpRequest();
+    request.open("POST", document.getElementById("url").value);
+    request.setRequestHeader('Content-type', 'application/json');
+    const params = {
+        username: document.getElementById("name").value,
+        avatar_url: "",
+        content: document.getElementById("content").value
+    };
+
+    request.send(JSON.stringify(params));
+}
