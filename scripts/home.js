bc228d11a3e69f3ce69deedf21801d19640eab30
aroary <71286008+aroary@users.noreply.github.com>
2022-01-31 23:21:20 -0500

    redo

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,1 @@
-const user = new XMLHttpRequest();
-user.open("GET", "https://api.github.com/users/aroary");
-user.setRequestHeader("Accept", "application/vnd.github.v3+json");
-user.onreadystatechange = () => {
-    if (user.readyState === 4) console.log(user.JSON.parse(user.responseText));
-};
-user.send();
+//
