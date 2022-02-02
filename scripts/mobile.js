f5fcd37ed2dee500361998ebdd2e8966288f863b
aroary <71286008+aroary@users.noreply.github.com>
2022-02-02 09:23:20 -0500

    ${

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,6 +1,6 @@
 function optomizeMarginForAgent(element, marinLeft, marginRight) {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         element.style.marginLeft = `${marinLeft}%`;
-        element.style.marginRight = `marginRight}%`;
+        element.style.marginRight = `${marginRight}%`;
     };
 };
