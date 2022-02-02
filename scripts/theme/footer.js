cad453ab54bf5099da877cac2c31d37304885136
aroary <71286008+aroary@users.noreply.github.com>
2022-02-02 12:11:13 -0500

    footer js

---
+++ scripts/theme/footer.js
@@ -0,0 +1,9 @@
+const items = [
+    document.body.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.clientHeight,
+    document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight   
+];
+
+document.getElementsByTagName("footer")[0].style.bottom = `${Math.max(...items)}px`;
