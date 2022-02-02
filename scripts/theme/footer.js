9156bb95f6909c91aeaee903c8c1c1156bbf312e
aroary <71286008+aroary@users.noreply.github.com>
2022-02-02 12:15:33 -0500

    footerfix?

--- scripts/theme/footer.js
+++ scripts/theme/footer.js
@@ -6,4 +6,4 @@ const items = [
     document.documentElement.offsetHeight   
 ];
 
-document.getElementsByTagName("footer")[0].style.bottom = `${Math.max(...items)}px`;
+document.getElementsByTagName("footer")[0].style.top = `${Math.max(...items)}px`;
