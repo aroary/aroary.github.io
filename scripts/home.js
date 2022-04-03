a8fcea2f3b170bdcb1534f7bbd5139374d6a1684
aroary <meanchemshkedi@gmail.com>
2022-04-02 23:35:47 -0400

    hover shadows

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,11 @@
 optomizeMarginForAgent(document.getElementsByTagName("main")[0], 10, 10);
+
+// about hover
+document.querySelector("main>div").onmouseover = () => {
+    document.querySelectorAll("main>div>h2").forEach(element => element.style.boxShadow = "inset 0 10px 6px -8px black, inset 0 -10px 6px -8px black");
+    document.querySelectorAll("div>p").forEach(element => element.style.boxShadow = "inset 0 0 8px black");
+};
+document.querySelector("main>div").onmouseout = () => {
+    document.querySelectorAll("main>div>h2").forEach(element => element.style.boxShadow = "inset 0 10px 6px -10px black, inset 0 -10px 6px -10px black");
+    document.querySelectorAll("div>p").forEach(element => element.style.boxShadow = "inset 0 0 6px black");
+};
