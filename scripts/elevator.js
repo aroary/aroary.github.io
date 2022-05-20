be41784f30174ee6f15c3d62aa8652f18802ccbb
aroary <meanchemshkedi@gmail.com>
2022-05-19 20:29:18 -0400

    double click option

--- scripts/elevator.js
+++ scripts/elevator.js
@@ -1,13 +1,19 @@
+var target = null;
+
 if (document.getElementById("elevator")) {
     document.getElementById("elevator").src = "./media/elevator/open.gif";
     [...document.getElementsByTagName("a")].forEach(element => {
         element.onclick = e => {
             e.preventDefault();
             document.getElementById("elevator").src = "./media/elevator/close.gif";
-            setTimeout(() => {
-                window.location.assign(element.href);
-                document.getElementById("elevator").src = "./media/elevator/open.gif";
-            }, 4000);
+            if (target === element.href) window.location.assign(target);
+            else {
+                target = element.href;
+                setTimeout(() => {
+                    window.location.assign(target);
+                    document.getElementById("elevator").src = "./media/elevator/open.gif";
+                }, 4000);
+            };
         };
     });
 };
