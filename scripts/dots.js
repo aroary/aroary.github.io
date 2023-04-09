d5182916d59a4277974a54baadacf9d947c87de3
aroary <me@aroary.com>
2023-04-09 11:40:44 -0400

    fix dot connectivity & add more mouse interaction

--- scripts/dots.js
+++ scripts/dots.js
@@ -3,39 +3,39 @@ const dots = document.getElementById("dots");
 const lineColor = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "darknavy" : "silver";
 const dotColor = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "silver" : "darknavy";
 
-window.onresize = () => {
-    dots.width = window.innerWidth;
-    dots.height = window.innerHeight;
-};
-
 const consistency = Math.floor(Math.random() * 4) + 6
 
 var ctx = dots.getContext("2d");
 var width = dots.width = window.innerWidth;
 var height = dots.height = window.innerHeight;
+var x = 0, y = 0;
 var rect = dots.getBoundingClientRect();
 var circles = [];
+
+window.onresize = () => {
+    width = dots.width = window.innerWidth;
+    height = dots.height = window.innerHeight;
+};
 
 for (var i = 0; i < consistency * 10; i++) circles.push({ x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height), d: Math.floor(Math.random() * 360) });
 dots.onclick = e => {
     circles.shift();
-    const dot = {
+    circles.push({
         x: (e.clientX - rect.left) / (rect.right - rect.left) * dots.width,
         y: (e.clientY - rect.top) / (rect.bottom - rect.top) * dots.height,
         d: Math.floor(Math.random() * 360)
-    };
-    circles.push(dot);
+    });
 };
+
+dots.onmousemove = e => {
+    x = e.offsetX;
+    y = e.offsetY;
+}
 
 ctx.strokeStyle = lineColor;
 ctx.stroke();
 
 setInterval(() => {
-    ctx = dots.getContext("2d");
-    width = dots.width;
-    height = dots.height;
-    rect = dots.getBoundingClientRect();
-
     ctx.clearRect(0, 0, width, height);
 
     circles = circles.map(circle => {
@@ -51,37 +51,23 @@ setInterval(() => {
         return circle;
     });
 
-    // Connect in order
-    // ctx.beginPath();
-    // ctx.moveTo(circles[0].x, circles[0].y);
-    // for (var i = 1; i < circles.length; i++) ctx.lineTo(circles[i].x, circles[i].y);
-    // ctx.lineTo(circles[0].x, circles[0].y);
-    // ctx.stroke();
+    // Add mouse position
+    const points = [{
+        x: (x - rect.left) / (rect.right - rect.left) * dots.width,
+        y: (y - rect.top) / (rect.bottom - rect.top) * dots.height,
+    }, ...circles];
 
-    // Connect to close
-    circles.forEach(circle => {
-        ctx.beginPath();
-        ctx.moveTo(circle.x, circle.y);
-        circles.filter(c => distance(c.x, c.y, circle.x, circle.y) < width / consistency).forEach(c => ctx.lineTo(c.x, c.y));
-        ctx.stroke();
+    // Draw lines
+    points.forEach(circle => {
+        circles.filter(c => distance(c.x, c.y, circle.x, circle.y) < width / consistency).forEach(c => {
+            ctx.beginPath();
+            ctx.moveTo(circle.x, circle.y);
+            ctx.lineTo(c.x, c.y);
+            ctx.stroke();
+        });
     });
 
-    // Connect to distant
-    // circles.forEach(circle => {
-    //     ctx.beginPath();
-    //     ctx.moveTo(circle.x, circle.y);
-    //     circles.filter(c => distance(c.x, c.y, circle.x, circle.y) > (width / consistency) * ((consistency - 1) || 1)).forEach(c => ctx.lineTo(c.x, c.y));
-    //     ctx.stroke();
-    // });
-
-    // Connect to all
-    // circles.forEach(circle => {
-    //     ctx.beginPath();
-    //     ctx.moveTo(circle.x, circle.y);
-    //     circles.forEach(c => ctx.lineTo(c.x, c.y));
-    //     ctx.stroke();
-    // });
-
+    // Draw circle
     circles.forEach(circle => {
         ctx.beginPath();
         ctx.arc(circle.x, circle.y, 3, 0, 2 * Math.PI);
